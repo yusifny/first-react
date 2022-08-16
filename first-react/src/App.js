8eaa5eda7fda22d0d2d68fc690e9f377e51b9ad5
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-08-17 02:55:14 +0400

    useState Hook

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 import './components/Employee'
 import Employee from './components/Employee';
 import ErrMsg from './components/ErrorMsg';
+import {useState} from 'react';
 
 function App() {
-  const showEmployee = false;
+  const [role, setRole] = useState();
+  const showEmployee = true;
   return (
     <div className="App">
     {showEmployee ? 
     (
       <>
+        <label>Add a role</label> <br/>
+        <input type="text" onChange={ (e) => {
+            setRole(e.target.value)
+        }}></input>
         <Employee name="Josip" surname="Laurel" age="30" role="CTO" />
-        <Employee name="Lorem" surname="Hesto" age="28" role="Senior Developer" />
-        <Employee name="John" surname="Harvich" age="22" role="Mid Developer"/>
+        <Employee name="John" surname="Hesto" age="28" role="Senior Developer" />
+        <Employee name="Lorem" surname="Harvich" age="22" role={role}/>
         <Employee name="Elen" surname="Soya" age="24"/>
       </>
     ) :
--- first-react/src/components/Employee.js
+++ first-react/src/components/Employee.js
@@ -3,8 +3,6 @@ function Employee(props) {
     return (
         <>
             <h3>{props.name}</h3>
-            <h3>{props.surname}</h3>
-            <h3>{props.age}</h3>
             <b>{props.role ? props.role : "No role"}</b>
             <hr></hr>
         </>
