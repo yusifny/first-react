16da4e4192461a822030674852b7b4e020532452
Yusif Nazarbayov <99797435+yusifny@users.noreply.github.com>
2022-08-17 14:32:32 +0400

    first-tailwind

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,31 +1,34 @@
-import './App.css';
-import './components/Employee'
-import Employee from './components/Employee';
-import ErrMsg from './components/ErrorMsg';
-import {useState} from 'react';
+import "./Index.css";
+import "./components/Employee";
+import Employee from "./components/Employee";
+import ErrMsg from "./components/ErrorMsg";
+import { useState } from "react";
 
 function App() {
   const [role, setRole] = useState();
   const showEmployee = true;
   return (
-    <div className="App">
-    {showEmployee ? 
-    (
-      <>
-        <label>Add a role</label> <br/>
-        <input type="text" onChange={ (e) => {
-            setRole(e.target.value)
-        }}></input>
-        <Employee name="Josip" surname="Laurel" age="30" role="CTO" />
-        <Employee name="John" surname="Hesto" age="28" role="Senior Developer" />
-        <Employee name="Lorem" surname="Harvich" age="22" role={role}/>
-        <Employee name="Elen" surname="Soya" age="24"/>
-      </>
-    ) :
-    <>
-      <ErrMsg />
-    </>
-    }
+    <div className="App bg-orange-300 px-10 pt-5 pb-8">
+      {showEmployee ? (
+        <>
+          <label className="text-lg">Add a role</label> <br />
+          <input
+            className="my-2 px-2 py-1 border-2 border-solid border-black rounded-md outline-0"
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          ></input>
+          <Employee name="Josip" role="CTO" />
+          <Employee name="John" role="Senior Developer" />
+          <Employee name="Lorem" role={role} />
+          <Employee name="Elen" />
+        </>
+      ) : (
+        <>
+          <ErrMsg />
+        </>
+      )}
     </div>
   );
 }
--- first-react/src/components/Employee.js
+++ first-react/src/components/Employee.js
@@ -1,12 +1,11 @@
 function Employee(props) {
-
-    return (
-        <>
-            <h3>{props.name}</h3>
-            <b>{props.role ? props.role : "No role"}</b>
-            <hr></hr>
-        </>
-    ) 
+  return (
+    <>
+      <h3>{props.name}</h3>
+      <b>{props.role ? props.role : "No role"}</b>
+      <hr></hr>
+    </>
+  );
 }
 
 export default Employee;
